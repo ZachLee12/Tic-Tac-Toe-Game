ce159ba51a644b2bf301b393ba1f1f6bf7d1a2ff
ZachLee12 <leezhengyang22@gmail.com>
2023-01-04 11:47:39 +0100

    Collect player info from form

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
 const gameBoard = (function () {
     const _board = ["", "", "", "", "", "", "", "", ""];
 
@@ -39,11 +53,21 @@ const createPlayer = function (name, team) {
 }
 
 const DOMCache = (function () {
-    //players info
-    const playerOneName = document.getElementById('player1-name').value
-    const playerTwoName = document.getElementById('player2-name').value
-    const playerOneTeam = document.getElementById('player1-team').value
-    const playerTwoTeam = document.getElementById('player2-team').value
+    const cachePlayersInfo = () => {
+        let playerOneName = document.getElementById('player1-name').value
+        let playerTwoName = document.getElementById('player2-name').value
+        let playerOneTeam = document.getElementById('player1-team').value
+        let playerTwoTeam = document.getElementById('player2-team').value
+        return {
+            playerOneName,
+            playerOneTeam,
+            playerTwoName,
+            playerTwoTeam
+        }
+    }
+
+    //form
+    const form = document.querySelector('.player-form')
 
     //DOM fields
     const DOMFields = (() => {
@@ -57,13 +81,14 @@ const DOMCache = (function () {
     //player's turn
     const playersTurnDiv = document.querySelector('.players-turn')
 
+    //play button
+    const playButton = document.querySelector('#play-btn');
     return {
-        playerOneName,
-        playerOneTeam,
-        playerTwoName,
-        playerTwoTeam,
         playersTurnDiv,
-        DOMFields
+        playButton,
+        cachePlayersInfo,
+        DOMFields,
+        form
     }
 })();
 
@@ -140,48 +165,36 @@ const gameOverChecker = (function () {
 
 //main IIFE module
 const gameController = (function () {
-    //players
-    const playerOne = createPlayer(DOMCache.playerOneName, DOMCache.playerOneTeam)
-    const playerTwo = createPlayer(DOMCache.playerTwoName, DOMCache.playerTwoTeam);
-    function placeCharOnField(field) {
-        if (field.innerHTML !== "") return;
-        if (playerOne.getPlayersTurn()) {
-            displayController.updateBoard(field, playerOne.getPlayerTeam(), playerTwo.getPlayerName())
-            playerOne.setPlayersTurn(false);
-            playerTwo.setPlayersTurn(true);
-        } else {
-            displayController.updateBoard(field, playerTwo.getPlayerTeam(), playerOne.getPlayerName())
-            playerOne.setPlayersTurn(true);
-            playerTwo.setPlayersTurn(false);
-        }
-    }
-    //initialize game
-    playerOne.setPlayersTurn(true);
-    DOMCache.DOMFields.forEach((field) => {
-        field.addEventListener('click', placeCharOnField.bind(this, field));
-        field.addEventListener('click', gameOverChecker.checkGameOver);
+    DOMCache.playButton.addEventListener('click', () => {
+        if (!DOMCache.form.checkValidity()) return;
+        const playerInfo = DOMCache.cachePlayersInfo()
+        initializeGame(playerInfo.playerOneName, playerInfo.playerOneTeam, playerInfo.playerTwoName, playerInfo.playerTwoTeam)
+        modal.style.display = 'none'
     })
+
+    function initializeGame(playerOneName, playerOneTeam, playerTwoName, playerTwoTeam) {
+        //players
+        const playerOne = createPlayer(playerOneName, playerOneTeam)
+        const playerTwo = createPlayer(playerTwoName, playerTwoTeam);
+        function placeCharOnField(field) {
+            if (field.innerHTML !== "") return;
+            if (playerOne.getPlayersTurn()) {
+                displayController.updateBoard(field, playerOne.getPlayerTeam(), playerTwo.getPlayerName())
+                playerOne.setPlayersTurn(false);
+                playerTwo.setPlayersTurn(true);
+            } else {
+                displayController.updateBoard(field, playerTwo.getPlayerTeam(), playerOne.getPlayerName())
+                playerOne.setPlayersTurn(true);
+                playerTwo.setPlayersTurn(false);
+            }
+        }
+        //initialize game
+        playerOne.setPlayersTurn(true);
+        DOMCache.DOMFields.forEach((field) => {
+            field.addEventListener('click', placeCharOnField.bind(this, field));
+            field.addEventListener('click', gameOverChecker.checkGameOver);
+        })
+    }
 })();
 
-
-
-
-
-
-
-
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
 
